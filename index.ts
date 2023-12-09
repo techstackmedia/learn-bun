f8598f4b926bb08626738b459b6d396dd9354824
Osagie Bello <osagiebello@Osagies-MacBook-Pro.local>
2023-12-09 20:10:33 +0100

    simple web server

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 const server = Bun.serve({
-    fetch(req: Request): Response | Promise<Response> {
-      return new Response("Hello People");
-    },
-  
-    // Optional port number - the default value is 3000
-    port: process.env.PORT || 3000,
-})
+  fetch(req: Request): Response | Promise<Response> {
+    const url = new URL(req.url);
+    if (url.pathname === '/') {
+      return new Response('This is the Home Page');
+    } else if (url.pathname === '/about') {
+      return new Response('About me!!!');
+    }
+    return new Response('404 error');
+  },
 
-console.log(`Lisening on port ${server.port}`)
-// bun index.ts 
+  // Optional port number - the default value is 3000
+  port: process.env.PORT || 3000,
+});
+
+console.log(`Lisening on port ${server.port}`);
+// bun index.ts
 // bun run index.ts
 // bun --watch index.ts
+// bun --hot index.ts
